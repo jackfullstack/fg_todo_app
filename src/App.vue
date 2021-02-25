b3cf926a725bea3f51e712a7357a080b0ef185f1
jackfullstack <ingurz@icloud.com>
2021-02-25 14:37:13 +0000

    SBUILD:AppComponent: add basic layout and styling with a navbar and main section where child components will be rendered

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <v-app>
-    TODOIT APP
+  <v-app id="FG_TODO_IT">
+    <v-app-bar color="orange" app>
+      <v-app-bar-title>FG_Todo_App</v-app-bar-title>
+    </v-app-bar>
+    <v-main></v-main>
   </v-app>
 </template>
 
