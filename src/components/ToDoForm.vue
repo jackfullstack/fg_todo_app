8eb59fb7e3d56819d93b8299c5fad7598f62a759
jackfullstack <ingurz@icloud.com>
2021-02-25 14:19:07 +0000

    BUILD: basic components added

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ToDoForm'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ToDOItem'
+    }
+</script>
+
+<style scoped>
+
+</style>
